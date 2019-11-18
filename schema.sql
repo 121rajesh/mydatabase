ea8570c7836b6b5f3d263b35ef0ce70fdda4afb4
Rajesh Zambre <rajeshzambre121@gmail.com>
2019-11-18 23:32:18 +0530

    Finally error resolved

--- schema.sql
+++ schema.sql
@@ -1,6 +1,5 @@
 
+create table emp1(S_No int(4),Name varchar(40),Address varchar(40));
 
-create table emp(S_No int(4),Name varchar(40),Address varchar(40));
-
-insert into emp values(1,"Rajesh","Indore"),(2,"Shreyas","Raigad"),(3,"Akash","Nagar"),
+insert into emp1 values(1,"Rajesh","Indore"),(2,"Shreyas","Raigad"),(3,"Akash","Nagar"),
 (4,"Swapnil","Nagpur");
